92752d47008e931d1ab9adbb8d53d2c378cf85aa
Andre Mesquita <avmesquita@gmail.com>
2026-08-19 12:43:15 -0300

    Add dependency injection support to Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Data;
 using System.Net;
 using System.Net.Mail;
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
